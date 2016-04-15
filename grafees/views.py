a4f68cdd9ec9ecfc56feb24655828fbdf772e64f
Sebastien PITTET <sebastien@pittet.org>
2016-04-15 14:35:05 +0000

    play with netatmo, first try with temp.

--- grafees/views.py
+++ grafees/views.py
@@ -3,6 +3,7 @@ from flask import redirect # redirect http
 import pygal
 from pygal.style import LightGreenStyle
 from grafees import app
+import lnetatmo
 
 @app.route('/')
 @app.route('/index')
@@ -29,3 +30,25 @@ def test():
     hist_chart.add('Padovan', [1, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12]) 
     chart = hist_chart.render(is_unicode=True)
     return render_template('chart.html', chart=chart )
+	
+@app.route('/netatmo')
+def current():
+    authorization = lnetatmo.ClientAuth()
+    devList = lnetatmo.DeviceList(authorization)
+
+    tempIndoor = devList.lastData()['Indoor']['Temperature']
+    tempOutdoor = devList.lastData()['Outdoor']['Temperature']
+
+    bar_chart = pygal.Bar(x_label_rotation=30,
+                          print_values=True,
+                          rounded_bars=2,
+                          style=LightGreenStyle,
+                          width=300,
+                          x_title = "Locations",
+                          y_title = "Current Degrees",
+                          legend_at_bottom=True)
+    bar_chart.title = "Temp. Ballaigues"
+    bar_chart.add('Indoor', tempIndoor)
+    bar_chart.add('Outdoor', tempOutdoor)
+    chart = bar_chart.render(is_unicode=True)
+    return render_template('chart.html', chart=chart)
