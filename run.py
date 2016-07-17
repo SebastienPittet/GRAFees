6a007a67aed249b08c4dd11b3d68b8975ca45c8e
Sebastien PITTET <sebastien@pittet.org>
2016-07-17 22:12:07 +0000

    ..

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 #!flask/bin/python
+
+# because of large applications
+# http://flask.pocoo.org/docs/0.11/patterns/packages/#larger-applications
+
 from grafees import app
 app.run(debug=True, host='0.0.0.0')
