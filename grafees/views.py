60cd3752453daf1ef1ed30bc333e118491957eea
Sebastien PITTET <sebastien@pittet.org>
2016-10-17 18:59:13 +0000

    removed the sample graph.

--- grafees/views.py
+++ grafees/views.py
@@ -23,22 +23,7 @@ def graph():
     return redirect('http://www.blog.pythonlibrary.org/2015/04/16/using-pygal-graphs-in-flask/')
 	# return pygal.Bar().add('1', [1, 3, 3, 7]).add('2', [1, 6, 6, 4]).render()
 
-@app.route('/chart')
-def test():
-    hist_chart = pygal.HorizontalStackedBar(Show_legend=True,
-                                            legend_box_size=18,
-                                            print_values=True,
-                                            rounded_bars=2,
-                                            style=LightGreenStyle)
-    hist_chart.title = "Remarquable sequences"
-    hist_chart.x_title = "Some Numbers"
-    hist_chart.x_labels = map(str, range(11))
-    hist_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
-    hist_chart.add('Padovan', [1, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12]) 
-    chart = hist_chart.render(is_unicode=True)
-    return render_template('chart.html', chart=chart )
-	
-@app.route('/netatmo')
+@app.route('/TempBallaigues')
 def currentTemp():
     authorization = lnetatmo.ClientAuth()
     devList = lnetatmo.DeviceList(authorization)
@@ -62,6 +47,8 @@ def currentTemp():
 @app.route('/AVGtemp', methods=('GET', 'POST'))
 def AVGtemp():
     # http://flask.pocoo.org/docs/0.11/patterns/wtforms/#in-the-view
+    
+    # Display a graph of Average Temperature at home
 
     selectIntervalle = grafees_forms.Intervalle()
     
@@ -77,7 +64,7 @@ def AVGtemp():
             
             for module, moduleData in dev.lastData(exclude=3600).items():
                 for sensor, value in moduleData.items():
-                    # Get Temperature and Humidity with GETMEASURE web service (1 sample every 30min)
+                    # Get Temperature with GETMEASURE web service (1 sample every 30min)
                     resp = dev.getMeasure( device_id='70:ee:50:05:cc:ac',   # Replace with your values  
                                             module_id='02:00:00:05:c2:96',  #    "      "    "    "
                                             scale="30min",
@@ -92,7 +79,6 @@ def AVGtemp():
             hist_chart = pygal.Bar(Show_legend=True,
                                         legend_box_size=18,
                                         print_values=False,
-                                        dynamic_print_values=True,
                                         rounded_bars=2,
                                         style=LightGreenStyle)
             hist_chart.title = u"Température à Ballaigues période %s and %s" % (str(selectIntervalle.dateFrom.data), str(selectIntervalle.dateTo.data))
@@ -101,9 +87,8 @@ def AVGtemp():
             hist_chart.add(u'Température °C', ytemp)
             chart = hist_chart.render().decode('utf-8')
 
-            debugText = str(sum(ytemp)/len(ytemp)) 
+            debugText = "" 
             return render_template('chart.html', chart=chart, debugText=debugText, title=u"Moyenne des températures")      
     debugText = ""    
     return render_template('form.html', form=selectIntervalle, debugText=debugText)
     
-
