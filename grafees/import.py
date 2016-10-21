46d37d742d05d683f35aba493f8cfe61d6e1a51b
Sebastien PITTET <sebastien@pittet.org>
2016-10-21 19:26:39 +0000

    import data from cave-link. No need of HTMLParser module.

--- grafees/import.py
+++ grafees/import.py
@@ -2,6 +2,7 @@
 
 from HTMLParser import HTMLParser
 from dateutil.parser import *
+import time
 
 # HTTP libraries depends upon Python 2 or 3
 from sys import version_info
@@ -11,27 +12,31 @@ else:
     from urllib import urlencode
     import urllib2
 
-the_url = 'http://www.cavelink.com/cl/da.php?s=106&g=1&w=101&l=20'
+the_url = 'http://www.cavelink.com/cl/da.php?s=106&g=1&w=101&l=10'
 webpage = urllib2.Request(the_url)
 
 def toEpoch(value):
-    return int(time.mktime(time.strptime(value,"%Y-%m-%d_%H:%M:%S")))
+    return int(time.mktime(time.strptime(value,"%Y-%m-%d %H:%M:%S")))
 
 def findDate(inputValue):
-    EpochDateTime = inputValue[0:16]
-    return EpochDateTime
+    try:
+        epochDateTime = parse(inputValue, ignoretz=True)
+    except:
+        epochDateTime = ""
+    return epochDateTime
 
 class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
         print data[:16], data[18:]
         dateTimeFound = parse(data, ignoretz=True)
+        return dateTimeFound
     
 try:
      handle = urllib2.urlopen(webpage)
 except IOError:
-     print 'Cela a foiré !'
+     print 'ERROR : unable to get the webpage :-/'
 else:
-    # Print the HTML page
+    # Get the HTML page
     htmlContent = handle.read()
     
     # Translate
@@ -39,6 +44,7 @@ else:
     htmlContent = htmlContent.replace("Stn=", "Station=")
     htmlContent = htmlContent.replace("Grp=", "Group=")
     htmlContent = htmlContent.replace("<br>", "\r\n")
+    
     
     # Finding spaces and replace by separator
     #htmlContent = htmlContent.replace(" ", "    ") # separator = 4 spaces?
@@ -48,11 +54,19 @@ else:
     
     #Separate Header from Data
     contentHeader = htmlContent[:htmlContent.index("=m")+2]
-    contentData = htmlContent[htmlContent.index("=m")+2:]   
-    print contentHeader
-    print contentData
-
+    contentData = htmlContent[htmlContent.index("=m")+5:]   
+    
+    lines = htmlContent.split("\r\n")
+    
+    for line in lines:
+        datetime = findDate(line[0:16])
+        if datetime:
+            print datetime, line[17:0]
+        else:
+            #skip the line
+            print "Error, no date found by parser :-/"
+            
+        
     
     # Besoin de python-dateutil ??
     # Besoin de HTMLParser ???
-    
