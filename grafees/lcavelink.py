1e1a60929be76d5614ec7887f56cf9ff48e8e440
Sebastien PITTET <sebastien@pittet.org>
2016-10-23 18:49:14 +0000

    Adaptation, pour faire de ce code une véritable librairie python.

--- grafees/lcavelink.py
+++ grafees/lcavelink.py
@@ -1,6 +1,13 @@
 #coding=utf-8
+# Published 2016
+# Author : sebastien at pittet dot org
+# Public domain source code
+"""
+This API provides access to Cave-Link data. It has some interest for cavers.
+Following libraries are required :
+    * python-dateutil
+"""
 
-from HTMLParser import HTMLParser
 from dateutil.parser import *
 import time
 
@@ -12,9 +19,69 @@ else:
     from urllib import urlencode
     import urllib2
 
-the_url = 'http://www.cavelink.com/cl/da.php?s=106&g=1&w=101&l=10'
-webpage = urllib2.Request(the_url)
+########################### Common definitions #########################
 
+_CL_NIVEAU_S2_COVA = "http://www.cavelink.com/cl/da.php?s=115&g=1&w=103&l=20"
+
+#########################################################################
+
+default_CL = _CL_NIVEAU_S2_COVA
+
+class GetCaveLinkData:
+    """
+    Parse the webpage used to export the data and
+    provides values back.
+    """
+
+    def __init__(self, URL = default_CL):
+        webpage = urllib2.Request(URL)
+        
+        try:
+            handle = urllib2.urlopen(webpage)
+        except IOError:
+            print ('ERROR : unable to get the webpage :-/')
+
+        # Get the HTML page
+        htmlContent = handle.read()
+    
+        # Translate
+        htmlContent = htmlContent.replace("Einheit", "Unit")
+        htmlContent = htmlContent.replace("Stn=", "Station=")
+        htmlContent = htmlContent.replace("Grp=", "Group=")
+        htmlContent = htmlContent.replace("<br>", "\r\n")
+        htmlContent = htmlContent.replace(",", "")
+        #htmlContent = htmlContent.replace(";:;", ":")
+        htmlContent = htmlContent.replace("Unit : m", "Unit=m")
+
+        #Separate Header from Data
+        self.contentHeader = htmlContent[:htmlContent.index("=m")+2]
+        self.contentData = htmlContent[htmlContent.index("=m")+5:]   
+
+    @property
+    def GetHeader(self):
+        
+        return self.contentHeader
+        
+    def GetData(self):
+        lines = self.contentData.split("\r\n")
+
+        for line in lines:
+            datetime = findDate(line[0:16])
+            if datetime:
+                print (datetime, line[17:0])
+            else:
+                #skip the line
+                print ("Error, no date found by parser :-/")
+        return 0
+
+    def handle_data(self, data):
+        print (data[:16], data[18:])
+        dateTimeFound = parse(data, ignoretz=True)
+        return dateTimeFound
+    
+    # Besoin de python-dateutil ??
+
+####################### SOME USEFUL TOOLS ###############################
 def toEpoch(value):
     return int(time.mktime(time.strptime(value,"%Y-%m-%d %H:%M:%S")))
 
@@ -24,49 +91,17 @@ def findDate(inputValue):
     except:
         epochDateTime = ""
     return epochDateTime
+######################################################################
 
-class MyHTMLParser(HTMLParser):
-    def handle_data(self, data):
-        print data[:16], data[18:]
-        dateTimeFound = parse(data, ignoretz=True)
-        return dateTimeFound
     
-try:
-     handle = urllib2.urlopen(webpage)
-except IOError:
-     print 'ERROR : unable to get the webpage :-/'
-else:
-    # Get the HTML page
-    htmlContent = handle.read()
-    
-    # Translate
-    htmlContent = htmlContent.replace("Einheit", "Unit")
-    htmlContent = htmlContent.replace("Stn=", "Station=")
-    htmlContent = htmlContent.replace("Grp=", "Group=")
-    htmlContent = htmlContent.replace("<br>", "\r\n")
-    
-    
-    # Finding spaces and replace by separator
-    #htmlContent = htmlContent.replace(" ", "    ") # separator = 4 spaces?
-    htmlContent = htmlContent.replace(",", "")
-    htmlContent = htmlContent.replace(";:;", ":")
-    htmlContent = htmlContent.replace("Unit : m", "Unit=m")
-    
-    #Separate Header from Data
-    contentHeader = htmlContent[:htmlContent.index("=m")+2]
-    contentData = htmlContent[htmlContent.index("=m")+5:]   
-    
-    lines = htmlContent.split("\r\n")
-    
-    for line in lines:
-        datetime = findDate(line[0:16])
-        if datetime:
-            print datetime, line[17:0]
-        else:
-            #skip the line
-            print "Error, no date found by parser :-/"
-            
-        
-    
-    # Besoin de python-dateutil ??
-    # Besoin de HTMLParser ???
+# auto-test when executed directly
+
+if __name__ == "__main__":
+
+    from sys import exit, stdout, stderr
+
+    # If launched interactively, display OK message
+    if stdout.isatty():
+        print("lcavelink.py : OK")
+
+    exit(0)
